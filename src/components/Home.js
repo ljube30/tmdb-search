562fa4bbacf39dbfaaf82d59a9f293c3433e4f56
Ljubisa Jerinic <ljubisajerinic@gmail.com>
2019-04-20 12:17:11 +0200

    Added nav, installed axios client

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
+import Nav from "./Nav";
 
 class Home extends Component {
   render() {
-    return <div>It works !</div>;
+    return (
+      <React.Fragment>
+        <Nav />
+      </React.Fragment>
+    );
   }
 }
 
---
+++ src/components/Nav.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+class Nav extends Component {
+  state = {
+    btn1: "btn btn-primary",
+    btn2: "btn btn-light"
+  };
+
+  handleClickShows = () => {
+    this.setState({
+      btn1: "btn btn-primary",
+      btn2: "btn btn-light"
+    });
+  };
+  handleClickMovies = () => {
+    this.setState({
+      btn1: "btn btn-light",
+      btn2: "btn btn-primary"
+    });
+  };
+  render() {
+    return (
+      <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item active" aria-current="page">
+            <button className={this.state.btn1} onClick={this.handleClickShows}>
+              Tv Shows
+            </button>
+          </li>
+          <li class="breadcrumb-item">
+            <button
+              className={this.state.btn2}
+              onClick={this.handleClickMovies}
+            >
+              Movies
+            </button>
+          </li>
+        </ol>
+      </nav>
+    );
+  }
+}
+
+export default Nav;
