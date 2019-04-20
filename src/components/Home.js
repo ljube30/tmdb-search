2791931716da3ecf98816f50067a0aac1968ab21
Ljubisa Jerinic <ljubisajerinic@gmail.com>
2019-04-20 14:14:07 +0200

    Pull top 10 tv shows from api

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,15 +3,18 @@ import Nav from "./Nav";
 import tmdb from "../apis/tmdb";
 
 class Home extends Component {
-  state = {};
-  searchTerm = 550;
-  onbuttonClick = async searchTerm => {
-    const response = await tmdb.get(`/movie/${this.searchTerm}`);
+  state = {
+    searchTerm: "/tv/popular",
+    data: []
+  };
+  componentDidMount = async searchTerm => {
+    const response = await tmdb.get(`${this.state.searchTerm}`);
     console.log(response);
     //we care for response.data.items because that is where api fetches videos
-    this.setState({});
+    this.setState({ data: response.data.results.splice(10) });
   };
   render() {
+    console.log(this.state.data);
     return (
       <React.Fragment>
         <Nav />
