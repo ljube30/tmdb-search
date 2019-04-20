d4eaa5175d803370f1c9bad3bf140e02a7e04fb6
Ljubisa Jerinic <ljubisajerinic@gmail.com>
2019-04-20 11:08:31 +0200

    Added bootstrap and react router packages

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from "./Home";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Route path="/" exact component={Home} />
+      </Router>
+    );
+  }
+}
+
+export default App;
