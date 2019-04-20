7d77fa25be37c886a312d73515438ddd4b0d6536
Ljubisa Jerinic <ljubisajerinic@gmail.com>
2019-04-20 13:53:20 +0200

    removed params for now

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,12 +4,9 @@ import tmdb from "../apis/tmdb";
 
 class Home extends Component {
   state = {};
+  searchTerm = 550;
   onbuttonClick = async searchTerm => {
-    const response = await tmdb.get("/movie/550", {
-      params: {
-        // q: searchTerm //search query (what we input in searchbar)
-      }
-    });
+    const response = await tmdb.get(`/movie/${this.searchTerm}`);
     console.log(response);
     //we care for response.data.items because that is where api fetches videos
     this.setState({});
