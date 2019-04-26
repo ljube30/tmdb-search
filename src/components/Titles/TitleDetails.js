76366ba19b9d24538c85bb6e1ebc8040f8a386a1
Ljubisa Jerinic <ljubisajerinic@gmail.com>
2019-04-26 15:14:31 +0200

    Added skeleton to TitleDetails

--- src/components/Titles/TitleDetails.js
+++ src/components/Titles/TitleDetails.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const TitleDetails = () => {
-  return <div>This is Title Details</div>;
+  return (
+    <div className="container">
+      <Link to="/">{"<"} Back</Link>
+      <div>Trailer Here</div>
+      <div>Title Here</div>
+      <div>Overview Here</div>
+    </div>
+  );
 };
 
 export default TitleDetails;
