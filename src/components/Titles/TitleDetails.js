e2849f63f6f2368dc57ad2a26e1895ea2777444c
Ljubisa Jerinic <ljubisajerinic@gmail.com>
2019-04-27 11:36:53 +0200

    Added api call to TitleDetails

--- src/components/Titles/TitleDetails.js
+++ src/components/Titles/TitleDetails.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import tmdb from "../../apis/tmdb";
 
-const TitleDetails = () => {
-  return (
-    <div className="container">
-      <Link to="/">{"<"} Back</Link>
-      <div>Trailer Here</div>
-      <div>Title Here</div>
-      <div>Overview Here</div>
-    </div>
-  );
-};
+class TitleDetails extends Component {
+  state = { data: [] };
+  titleId = localStorage.getItem("titleId");
+
+  componentDidMount = async () => {
+    let response = {};
+    try {
+      response = await tmdb.get(`/movie/${this.titleId}`);
+    } catch {
+      response = await tmdb.get(`/tv/${this.titleId}`);
+    }
+    this.setState({ data: response.data });
+  };
+
+  render() {
+    console.log(this.state.data);
+    return (
+      <div className="container">
+        <Link to="/">{"<"} Back</Link>
+        <div>Trailer Here</div>
+        <div>Title Here</div>
+        <div>Overview Here</div>
+      </div>
+    );
+  }
+}
 
 export default TitleDetails;
